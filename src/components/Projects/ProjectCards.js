49c07145ca1e9e582df2065878038b3f4533f471
vikash verma <vikashvermacom92@gmail.com>
2024-02-06 17:34:51 +0530

    'update'

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -7,7 +7,9 @@ import { BsGithub } from "react-icons/bs";
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+
+      <Card.Img variant="top" src={props.imgPath} alt="card-img" style={{ width: '100%', height: 'auto' }}/>
+
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
--- src/components/Projects/constant.js
+++ src/components/Projects/constant.js
@@ -1,6 +1,7 @@
-import shoppinghub from "./Projects/shoppinghub.jpg"
-import agrointel from "./Projects/agrointel.png"
-import trends from "./Projects/trends-3.png"
+import shoppinghub from "../../Assets/Projects/shoppinghub.jpg"
+import agrointel from "../../Assets/Projects/agrointel.png"
+import trends from "../../Assets/Projects/trends-1.png"
+import amazon from "../../Assets/Projects/amazon.png"
 
 export default {
     "projects": [
@@ -8,30 +9,35 @@ export default {
             "title":"Shopping Hub",
             "imgPath": shoppinghub ,
             "description": "ShoppingHub is a feature-rich React Native ecommerce application that provides a seamless shopping experience for users. It comes with a robust authentication system integrated with Google and Firebase, as well as versatile payment options including Stripe and UPI.",
-            "skills": ["React Native" ],
+            "skills": ["React Native" , "Firebase" , "Stripe Payment", "Google Auth"  ],
             "ghLink": "https://github.com/dev-vikash2v7/ShoppingHub-RN-Ecom-App",
             "demoLink" : "https://drive.google.com/file/d/1l5tWOJ5LfTMbKi9q2ZusYJwYjXHJjpH_/view?usp=drive_link"
         },
         {
             "title":"AgroIntel Pro",
             "imgPath": agrointel,
-            "description": "Deployed an Ecommerce site with  Django as Backend and Paytm supported payment gateway.",
-            "skills": ["Django",   "HTML/CSS"   , "JavaScripts" , "MySQL"],
-            "ghLink": "https://github.com/vikash129/Django-vk_GalaxyStore"
+            "description": "AgroIntel Pro is a Smart Farming Mobile Application is a cutting-edge approach to farming that leverages technology to optimize crop production while conserving resources. This project report discusses the development and implementation of a Smart Crop, Fertilizer, and Disease Predictor System.",
+            "skills": ["React Native"  , "JavaScripts" ,"Python", "Machine Learning"],
+            "ghLink": "https://github.com/dev-vikash2v7/AgroIntelPro-RN-App",
+            "demoLink" : "https://drive.google.com/file/d/1lb27OthyK7G_qXZ6Tlf-pv3zxKuFTxff/view?usp=sharing"
+
         }, 
         {
             "title":"Trends - Ecommerce platform",
             "imgPath": trends,
-            "description": "Deployed an Ecommerce site with  Django as Backend and Paytm supported payment gateway.",
-            "skills": ["Django",   "HTML/CSS"   , "JavaScripts" , "MySQL"],
-            "ghLink": "https://github.com/vikash129/Django-vk_GalaxyStore"
+            "description": "Trends is a modern and user-friendly e-commerce website built using the MERN stack (MongoDB, Express.js, React.js, Node.js). The website allows users to explore trending products and make purchases securely using the Stripe payment system.",
+            "skills": ["NodeJs" , "MongoDB" , "Express.JS" , "ReactJs"  , "Stripe"],
+            "ghLink": "https://github.com/dev-vikash2v7/TRENDS--ReactJS-Ecommerce-Website",
+            "demoLink" : "https://trends-ecommerce.vercel.app"
+
         },
         {
-            "title":"AgroIntel Pro",
-            "imgPath": 'agrointel',
-            "description": "Deployed an Ecommerce site with  Django as Backend and Paytm supported payment gateway.",
-            "skills": ["Django",   "HTML/CSS"   , "JavaScripts" , "MySQL"],
-            "ghLink": "https://github.com/vikash129/Django-vk_GalaxyStore"
+            "title":"Amazon Clone",
+            "imgPath": amazon,
+            "description": "This Amazon-clone is a full-stack e-commerce website created using the MERN Stack (MongoDB, Express, React, Node.js). ",
+            "skills": ["ReactJs" , "HTML" , "CSS" , "JavaScript" , "Firebase"],
+            "ghLink": "https://github.com/dev-vikash2v7/amazon-clone",
+            "demoLink" : "https://vk-amazon-clone.netlify.app/"
         },
     ]
 
